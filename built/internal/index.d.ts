1ed824bc20bf622f7aa239a5b6bcccfe8d171054
NoriDev <m1nthing2322@gmail.com>
2023-12-18 16:42:40 +0900

    chore

--- built/internal/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as M from '..';
-export type FullParserOpts = {
-    nestLimit?: number;
-};
-export declare function fullParser(input: string, opts: FullParserOpts): M.MfmNode[];
-export declare function simpleParser(input: string): M.MfmSimpleNode[];
